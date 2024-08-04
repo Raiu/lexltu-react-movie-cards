aa5c0ec148c927da3f2cebb6c40bb509858a8505
Viktor Näslund <1299835+Raiu@users.noreply.github.com>
2024-08-04 15:15:28 +0200

    formating

--- src/components/Movies/Movies.tsx
+++ src/components/Movies/Movies.tsx
@@ -5,19 +5,18 @@ import {
     useContext,
     useReducer,
 } from "react";
+
 import {
     IMovie,
     IMoviesAction,
     IMoviesProviderProps,
     TMoviesAction,
 } from "@interfaces";
+
 import { movies as initialMovies } from "@data";
 
-const MoviesContext = createContext<IMovie[] | null>(null);
-
-const MoviesDispatchContext = createContext<Dispatch<IMoviesAction> | null>(
-    null
-);
+const MoviesContext = createContext<IMovie[]>([] as IMovie[]);
+const MoviesDispatchContext = createContext<Dispatch<IMoviesAction> | null>(null);
 
 export function Movies({ children }: IMoviesProviderProps): ReactElement {
     const [movies, action] = useReducer(moviesReducer, initialMovies);
@@ -48,12 +47,16 @@ export function moviesReducer(
     return movies;
 }
 
-export function useMovies(): IMovie[] | null {
+export function useMovies(): IMovie[] {
     return useContext(MoviesContext);
 }
 
-export function useMoviesDispatch(): Dispatch<IMoviesAction> | null {
-    return useContext(MoviesDispatchContext);
+export function useMoviesDispatch(): Dispatch<IMoviesAction> {
+    const dispatch = useContext(MoviesDispatchContext);
+    if (!dispatch) {
+        throw new Error("useMoviesDispatch must be used within a MoviesProvider");
+    }
+    return dispatch;
 }
 
 function createMovie(movie: IMovie, movies: IMovie[]): IMovie {
